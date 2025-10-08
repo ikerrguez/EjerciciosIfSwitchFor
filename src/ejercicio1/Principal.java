6d936004572562504bc8a43b71948b00cfc993be
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-08 09:25:43 +0200

    Ejercicio 1 acabado

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,7 +1,19 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Principal {
     static void main() {
-        
+        System.out.println("Dime la velocidad actual de tu coche en km/h");
+        Scanner sc = new Scanner(System.in);
+        int velocidad = sc.nextInt();
+        boolean esExcesoVelocidad= true;
+        if (velocidad >120) {
+            System.out.println("¡Atención! Vas a una velocidad excesiva. ¡Riesgo de multa!");
+        }else{
+        esExcesoVelocidad=false;{
+                System.out.println("Velocidad adecuada. !Sigue así y conduce con seguridad!");
+            }
+        }
     }
 }
