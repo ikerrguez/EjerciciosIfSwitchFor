39ee6fee7d554857bee4962d0af9a276b2b2d42e
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-13 19:56:01 +0200

    Ejercicio 7 acabado

--- src/ejercicio7/Principal.java
+++ src/ejercicio7/Principal.java
@@ -4,54 +4,56 @@ import java.util.Scanner;
 
 public class Principal {
     static void main() {
-        System.out.println("¡COMIENZA LA CARRERA DE DRAG DE 400 METROS!");
-        Scanner sc = new Scanner(System.in);
 
         String coche1 = "Muscle Car";
+        String coche2 = "Deportivo Japonés";
+
         double velocidad1 = 0.0;
-        double velocidadNueva1 = 0.0;
-        double distancia1 = 0.0;
-        double distanciaNueva1 = 0.0;
-        double aceleracion1 = 5.5;
-
-        String coche2 = "Deportivo japonés";
         double velocidad2 = 0.0;
-        double velocidadNueva2 = 0.0;
+        double distancia1 = 0.0;
         double distancia2 = 0.0;
-        double distanciaNueva2 = 0.0;
+
+        double aceleracion1 = 5.5;
         double aceleracion2 = 6.0;
+        double meta = 400.0;
 
-        for (int i = 1; i <= 15; i++) {
-            velocidadNueva1 += velocidad1 + aceleracion1;
-            velocidadNueva2 += velocidad2 + aceleracion2;
-            distanciaNueva1 += distancia1 + velocidadNueva1;
-            distanciaNueva2 += distancia2 + velocidadNueva2;
-            System.out.println("En el segundo " + i + " : " + coche1 + " ha recorrido " + distanciaNueva1 + " metros  | " + coche2 + " ha recorrido " + distanciaNueva2 + " metros");
+        System.out.println("¡COMIENZA LA CARRERA DE DRAG DE 400 METROS!");
+
+        boolean coche1Cruzo = false;
+        boolean coche2Cruzo = false;
+
+        for (int segundo = 1; segundo <= 15; segundo++) {
+
+            velocidad1 += aceleracion1;
+            velocidad2 += aceleracion2;
+
+            distancia1 += velocidad1;
+            distancia2 += velocidad2;
+
+            System.out.println("Segundo " + segundo + ": " + coche1 + " ha recorrido " + distancia1 + " metros | " + coche2 + " ha recorrido " + distancia2 + " metros");
 
 
-            if (distancia1 >= 400 || distancia2 >= 400) {
-                if (distancia1 >= distancia2) {
-                    System.out.println("Gana el Muscle Car");
-                }else if (distancia2 >= distancia1) {
-                    System.out.println("Gana el Deportivo Japonés");
-                }else{
-                    System.out.println("Empate");
-                }
-                break;
-
+            if (distancia1 >= meta && !coche1Cruzo) {
+                System.out.println("¡El " + coche1 + " ha cruzado la meta!");
+                coche1Cruzo = true;
+            }
+            if (distancia2 >= meta && !coche2Cruzo) {
+                System.out.println("¡El " + coche2 + " ha cruzado la meta!");
+                coche2Cruzo = true;
             }
 
+            if (coche1Cruzo && coche2Cruzo) {
+                break;
+            }
         }
 
-
+        System.out.print("¡FIN DE LA CARRERA! ");
+        if (distancia1 > distancia2) {
+            System.out.println("El ganador es el " + coche1 + ".");
+        } else if (distancia2 > distancia1) {
+            System.out.println("El ganador es el " + coche2 + ".");
+        } else {
+            System.out.println("¡Empate!");
+        }
     }
 }
-
-
-
-
-
-
-
-
-
